c4a31574327aa643d7a573c4bbe8b6cffed63a61
RachedSouihi <Souihiraached@gmail.com>
2025-10-07 15:17:23 +0100

    Update axios version to 1.12.0

--- frontend/src/api/contact.ts
+++ frontend/src/api/contact.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'https://portfolio-backend-vo7e.onrender.com/contact';
 
 export const submitContactForm = async (formData: any) => {
   try {
     console.log('Submitting form data:', formData);
-    const response = await axios.post(API_URL, formData);
+    const response = await axios.post(process.env.VITE_API_URL!, formData);
     return response.data;
   } catch (error) {
     console.error('Error submitting contact form:', error);
